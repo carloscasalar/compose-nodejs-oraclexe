223f0203f8b8bd68c0babad89cdaf4c35a91ca6c
Carlos Castillo Alarcón <sir.ragnar@gmail.com>
2015-10-16 23:45:28 +0200

    Undone attempt to unlock hr account while building Docker image. Dockerfile for oracle xe image no longer needed

--- images/oracle-xe/unlockhr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#export PATH=/u01/app/oracle/product/11.2.0/xe/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-#export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
-#export ORACLE_SID=XE
-echo unlocking hr account
-/bin/su - oracle -c "$ORACLE_HOME/bin/sqlplus -S system/oracle @/u01/scripts/init/unlockhr.sql"
